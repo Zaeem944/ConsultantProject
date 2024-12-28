2416dc6aa25623134b0883671e4951c9891e7f8e
Zaeem944 <25100071@lums.edu.pk>
2024-12-29 01:18:36 +0500

    added Hero component

---
+++ ConsultantProject/src/components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Hero.css'
+import arrow from '../../assets/edusity_assets/dark-arrow.png'
+
+const Hero = () => {
+  return (
+    <div className='hero container'> 
+      <div className="hero-text">
+        <h1>Ask Away !!!!</h1>
+        <p>Ask our student councelors that will assits you in your college application journey</p>
+        <button className='btn features'>Get Started <img src={arrow} alt="" /></button>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
--- ConsultantProject/src/components/Navbar/Navbar.jsx
+++ ConsultantProject/src/components/Navbar/Navbar.jsx
@@ -4,14 +4,14 @@ import logo from '../../assets/edusity_assets/logo.png'
 
 const Navbar = () => {
   return (
-    <nav>
-      <img src={logo} alt="" />
+    <nav className='container'>
+      <img src={logo} alt="" className='logo'/>
       <ul>
         <li>Home</li>
-        <li>Prgram</li>
+        <li>Program</li>
         <li>Features</li>
-        <li>Services</li>
-        <li>About Us</li>
+        <li >Services</li>
+        <li><button className='btn'>Contact Us</button></li>
       </ul>
     </nav>
   )
