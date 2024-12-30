37ced56b16308225fdfeb359599e90161e2010ec
Zaeem944 <25100071@lums.edu.pk>
2024-12-30 23:13:37 +0500

    added other features

--- ConsultantProject/src/App.jsx
+++ ConsultantProject/src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
+import Programs from './components/Programs/Programs'
+import Title from './components/Title/Title'
+import About from './components/About/About'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <div className="container">
+        <Title />
+        <Programs />
+        <About />
+        </div>
     </div>
   )
 }
---
+++ ConsultantProject/src/components/About/About.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './About.css'
+import about_img from '../../assets/edusity_assets/about.png'
+import play_icon from '../../assets/edusity_assets/play-icon.png'
+
+const About = () => {
+  return (
+    <div className='about' >
+        <div className="about-left">
+            <img src={about_img} alt="" className='about-img'/>
+            <img src={play_icon} alt="" className='play-icon'/>
+        </div>
+        <div className="about-right">
+            <h3>ABOUT UNIVERSITY</h3>
+            <h2>Lorem ipsum dolor sit amet consectetur.
+            </h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint, accusamus dolor consequatur aliquam sequi eius dolores, quia reprehenderit porro molestias ex nisi odio dolorum iusto quaerat consequuntur ratione praesentium quo.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit vel ipsum tempora iure, impedit a temporibus unde placeat, mollitia sed corporis nesciunt porro ab possimus maiores, perspiciatis assumenda voluptas dicta doloremque provident consequatur obcaecati? Ipsam nisi eos omnis autem minus?</p>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default About
