ee16b290dce43ae0750bb6b020267836a2bf3909
Zaeem944 <25100071@lums.edu.pk>
2025-01-11 00:28:47 +0500

    Update Testimonials.jsx

--- ConsultantProject/src/components/Testimonials/Testimonials.jsx
+++ ConsultantProject/src/components/Testimonials/Testimonials.jsx
@@ -46,7 +46,7 @@ const Testimonials = () => {
                             <p><b>SAT:</b> 1500 </p>
                         </div>
                     </div>
-                    <p>I had the privilege of being Bilal's first student, and his guidance truly transformed my SAT journey. Thanks to his personalized teaching methods, I achieved an impressive score above 1500, which opened doors to incredible opportunities. His teaching was highly adaptive and tailored perfectly to my learning style and pace, making even the most challenging concepts easy to grasp. Beyond the SAT, he was a true mentor, supporting me through the complex university admissions process, ultimately landing me into LUMS. His dedication and guidance make him an exceptional tutor for anyone aiming to achieve their academic and career goals.</p>
+                    <p>I had the privilege of being zaraya education's first student, and their guidance truly transformed my SAT journey. Thanks to their personalized teaching methods, I achieved an impressive score above 1500, which opened doors to incredible opportunities. Their teaching was highly adaptive and tailored perfectly to my learning style and pace, making even the most challenging concepts easy to grasp. Beyond the SAT, they were a true mentor, supporting me through the complex university admissions process, ultimately landing me into LUMS. Their dedication and guidance made them an exceptional tutor for anyone aiming to achieve their academic and career goals.</p>
                 </div>
             </li>
             <li>
@@ -60,7 +60,7 @@ const Testimonials = () => {
                             <p><b>SAT:</b> 1520 </p>
                         </div>
                     </div>
-                    <p>I highly recommend Sir Bilal for SAT prep. His teaching style is both thorough and engaging, breaking down complex concepts in a way that’s easy to understand. He tailored his approach to my strengths and weaknesses, providing strategies that significantly improved my performance. Thanks to his guidance, I was able to achieve a 1520 on my SAT. His dedication and personalized approach, in not only in SAT, made college admissions and university essays made a real difference in my prep and my university acceptances.</p>
+                    <p>I highly recommend Zaraya Education for SAT prep. Their teaching style is both thorough and engaging, breaking down complex concepts in a way that’s easy to understand. They tailored their approach to my strengths and weaknesses, providing strategies that significantly improved my performance. Thanks to their guidance, I was able to achieve a 1520 on my SAT. Their dedication and personalized approach, in not only in SAT, made college admissions and university essays made a real difference in my prep and my university acceptances.</p>
                 </div>
             </li>
             <li>
@@ -73,7 +73,7 @@ const Testimonials = () => {
                             <p> <b>School : </b>Beaconhouse Bahria Town</p>
                         </div>
                     </div>
-                    <p>I am deeply grateful to Sir Bilal for his invaluable guidance and support in preparing me for college applications. His clear explanations, personalized teaching approach, and constant encouragement made a challenging process much more manageable. Thanks to his efforts, I was able to achieve a strong score, which played a significant role in helping me secure admission to my dream university, Florida International University (FIU). Your dedication to my success has been truly inspiring, and I cannot thank you enough for your effort and patience in guiding me through this journey.</p>
+                    <p>I am deeply grateful to Zaraya Education for their invaluable guidance and support in preparing me for college applications. Their clear explanations, personalized teaching approach, and constant encouragement made a challenging process much more manageable. Thanks to their efforts, I was able to achieve a strong score, which played a significant role in helping me secure admission to my dream university, Florida International University (FIU). Their dedication to my success has been truly inspiring, and I cannot thank you enough for their effort and patience in guiding me through this journey.</p>
                 </div>
             </li>
             <li>
