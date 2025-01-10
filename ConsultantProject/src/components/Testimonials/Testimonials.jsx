d83c28a1c9f796c3ae1a92f82b7b32f32401c142
Zaeem944 <25100071@lums.edu.pk>
2025-01-10 12:31:42 +0500

    more detail on testimonials

--- ConsultantProject/src/components/Testimonials/Testimonials.jsx
+++ ConsultantProject/src/components/Testimonials/Testimonials.jsx
@@ -41,9 +41,8 @@ const Testimonials = () => {
                         <img src={user_1} alt="" />
                         <div>
                             <h3>Muhammad Arslan Ali</h3>
-                            <span>Florida International University</span>
-                            <span>Beaconhouse Bahria Town</span>
-                            <span>SAT:</span>
+                            <p> <b>University : </b>Florida International University</p>
+                            <p> <b>School : </b>Beaconhouse Bahria Town</p>
                         </div>
                     </div>
                     <p>I am deeply grateful to Sir Bilal for his invaluable guidance and support in preparing me for college applications. His clear explanations, personalized teaching approach, and constant encouragement made a challenging process much more manageable. Thanks to his efforts, I was able to achieve a strong score, which played a significant role in helping me secure admission to my dream university, Florida International University (FIU). Your dedication to my success has been truly inspiring, and I cannot thank you enough for your effort and patience in guiding me through this journey.</p>
@@ -55,7 +54,9 @@ const Testimonials = () => {
                         <img src={user_2} alt="" />
                         <div>
                             <h3>Muhammad Haseeb Khan</h3>
-                            <span>University of Texas, Arlington </span>
+                            <p> <b>University : </b>University of Texas, Arlington </p>
+                            <p> <b>School : </b>Beaconhouse Defense Campus</p>
+                            <p><b>SAT:</b> 1520 </p>
                         </div>
                     </div>
                     <p>I highly recommend Sir Bilal for SAT prep. His teaching style is both thorough and engaging, breaking down complex concepts in a way that’s easy to understand. He tailored his approach to my strengths and weaknesses, providing strategies that significantly improved my performance. Thanks to his guidance, I was able to achieve a 1520 on my SAT. His dedication and personalized approach, in not only in SAT, made college admissions and university essays made a real difference in my prep and my university acceptances.</p>
@@ -67,7 +68,9 @@ const Testimonials = () => {
                         <img src={user_3} alt="" />
                         <div>
                             <h3>Ayesha Jamal</h3>
-                            <span>LUMS</span>
+                            <p> <b>University : </b>LUMS</p>
+                            <p> <b>School : </b>International School Lahore</p>
+                            <p><b>SAT:</b> 1500 </p>
                         </div>
                     </div>
                     <p>I had the privilege of being Bilal's first student, and his guidance truly transformed my SAT journey. Thanks to his personalized teaching methods, I achieved an impressive score above 1500, which opened doors to incredible opportunities. His teaching was highly adaptive and tailored perfectly to my learning style and pace, making even the most challenging concepts easy to grasp. Beyond the SAT, he was a true mentor, supporting me through the complex university admissions process, ultimately landing me into LUMS. His dedication and guidance make him an exceptional tutor for anyone aiming to achieve their academic and career goals.</p>
