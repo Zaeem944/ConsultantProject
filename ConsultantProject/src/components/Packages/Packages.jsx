6d9c16fcc3f40f3a3cf055523808c604dc36a480
Zaeem944 <25100071@lums.edu.pk>
2025-01-12 15:43:51 +0500

    change of images

--- ConsultantProject/src/components/Packages/Packages.jsx
+++ ConsultantProject/src/components/Packages/Packages.jsx
@@ -16,7 +16,8 @@ const Packages = () => {
               <li>4 classes per week, 1 hour each</li>
               <li>Flexible Weekend/Weekday Timings</li>
               <li>Personalized 1:1 classes</li>
-              {/* <li>Price: PKR 25,000</li> */}
+              <li>Detailed Notes, Books, Past Exams</li>
+              <li><b>New Year Discounted Price:</b> PKR 24,999</li>
             </ul>
           </div>
           <div className="package-item">
@@ -27,7 +28,9 @@ const Packages = () => {
               <li>Includes core concepts and strategies (English + Math Section)</li>
               <li>In-depth practice questions from past exams/papers</li>
               <li>4 classes per week, 1 hour each</li>
+              <li>Detailed Notes, Books, Past Exams</li>
               <li>Fixed Timings</li>
+              <li><b>New Year Discounted Price:</b> PKR 14,999</li>
             </ul>
           </div>
         </div>
@@ -35,13 +38,13 @@ const Packages = () => {
         <div className="package">
           <h2>University Essays and Personal Statement Packages</h2>
           <div className="package-item">
-            <h3>Personal Statement Plus Package</h3>
+            <h3>Personal Statement/Individual Essay Plus Package</h3>
             <ul>
               <li>3 Sessions, 1 hour each</li>
-              <li>Session 1: Brainstorming and crafting essay structure</li>
+              <li>Session 1: How to brainstorm and craft the perfect essay structure</li>
               <li>Session 2: First Draft Review</li>
               <li>Session 3: Second Draft Review + Edits</li>
-              {/* <li>Price: PKR 2,500</li> */}
+              <li><b>New Year Discounted Price:</b> PKR 4,999</li>
             </ul>
           </div>
           <div className="package-item">
@@ -49,8 +52,8 @@ const Packages = () => {
             <ul>
               <li>Continuous and ongoing consultation support</li>
               <li>Covers all types of essays (Personal Statement, Extra Curricular, Supplemental)</li>
-              <li>Unlimited draft review and edits</li>
-              {/* <li>Price: PKR 10,000</li> */}
+              <li>Triple Sessions Individually for each essay</li>
+              <li><b>New Year Discounted Price:</b> PKR 8,499</li>
             </ul>
           </div>
         </div>
@@ -61,32 +64,40 @@ const Packages = () => {
             <h3>One Time Consultation (LUMS)</h3>
             <ul>
               <li>Application strategies and academic major consultation</li>
-              <li>Extra-curricular planning</li>
-              <li>Overall profile improvement</li>
+              <li>How to stand out through your extracurriculars and awards</li>
+              <li>Overall profile review and suggestions</li>
               <li>1-hour session</li>
-              {/* <li>Price: PKR 1,500</li> */}
-            </ul>
-          </div>
-          <div className="package-item">
-            <h3>Premium University Package (1 Month)</h3>
-            <ul>
-              <li>Continuous and ongoing support</li>
-              <li>Application strategies, academic major consultation, and profile improvement</li>
-              <li>Unlimited draft reviews and edits</li>
-              {/* <li>Price: PKR 15,000</li> */}
+              <li><b>New Year Discounted Price:</b> PKR 3,499</li>
             </ul>
           </div>
         </div>
         <div className="package">
           <h2>Combined Packages</h2>
           <div className="package-item">
-          <h3>Premium Essay  and Consultation Package (1 Month)</h3>
+          <h3>Premium Consultation Package (1 Month)</h3>
             <ul>
-              <li>Continuous and ongoing consultation support</li>
-              <li>Covers all types of essays (Personal Statement, Extra Curricular, Supplemental)</li>
+              <li>Continuous and ongoing support</li>
               <li>Application strategies, academic major consultation, and profile improvement</li>
-              <li>Unlimited draft review and edits</li>
-              {/* <li>Price: PKR 10,000</li> */}
+              <li>How to stand out through your extracurriculars and awards</li>
+              <li>Covers all types of essays (Personal Statement, Extra Curricular, Supplemental)</li>
+              <li>Triple Sessions Individually for each essay (Session 1: How to brainstorm and craft the perfect essay structure, Session 2: First Draft Review, Session 3: Updated Draft Review + Final Edits)</li>
+              <li><b>New Year Discounted Price:</b> PKR 9,999</li>
+            </ul>
+          </div>
+          <div className="package-item">
+            <h3>Premium University Package (2 Month)</h3>
+            <ul>
+              <li>Complete SAT Prep (English + Math Section)</li>
+              <li>Includes core concepts and strategies</li>
+              <li>Includes in-depth practice questions from past exams/past papers </li>
+              <li>4 classes per week, 1 hour each, Flexible Weekend/Weekday Timings</li>
+              <li>Personalized 1:1 classes</li>
+              <li>Continuous and Ongoing Support throughout the application process</li>
+              <li>Application strategies, academic major consultation, and profile improvement</li>
+              <li>How to stand out through your extracurriculars and awards</li>
+              <li>Covers all types of Essays: Personal Statement + Extra Curricular + Supplemental/Why Us/Why Academic Programmed</li>
+              <li>Triple Sessions Individually for each essay (Session 1: How to brainstorm and craft the perfect essay structure Session 2: First Draft Review Session 3: Updated Draft Review + Final Edits)</li>
+              <li><b>New Year Discounted Price:</b> PKR 49,999</li>
             </ul>
           </div>
         </div>
--- ConsultantProject/src/components/Programs/Programs.jsx
+++ ConsultantProject/src/components/Programs/Programs.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import './Programs.css';
 import program_1 from '../../assets/edusity_assets/program-1.2.png';
-import program_2 from '../../assets/edusity_assets/program-2.png';
-import program_3 from '../../assets/edusity_assets/program-3.png';
+import program_2 from '../../assets/edusity_assets/program-2.2.png';
+import program_3 from '../../assets/edusity_assets/program-3.2.png';
 import program_icon_1 from '../../assets/edusity_assets/program-icon-1.2.png';
 import program_icon_2 from '../../assets/edusity_assets/program-icon-2.png';
 import program_icon_3 from '../../assets/edusity_assets/program-icon-3.png';
